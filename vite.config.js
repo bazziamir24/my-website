37aefb865a0d5a36e70b17648f477e847538038b
Amir Bazzi <bazziamir@outlook.com>
2025-09-24 09:19:33 +0200

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/my-website/',
+  base: '/',
+  esbuild: {
+    loader: 'jsx',                 // <-- parse JSX in .js files
+    include: /src\/.*\.js$/,       // only your src/*.js files
+  },
 })
